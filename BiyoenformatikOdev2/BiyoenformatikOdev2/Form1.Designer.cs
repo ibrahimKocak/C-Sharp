f02196d66f5f9c8331faf66ae61d5b4ad2eac568
ibrahimKocak <ibrahimkocak93@gmail.com>
2017-10-11 14:07:31 +0300

    Bioenformatic
    Apps done

---
+++ BiyoenformatikOdev2/BiyoenformatikOdev2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace BiyoenformatikOdev2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(10, 40);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(355, 220);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(370, 40);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(355, 220);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.Location = new System.Drawing.Point(700, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(24, 12);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Help";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel2.Location = new System.Drawing.Point(703, 263);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(30, 12);
+            this.linkLabel2.TabIndex = 4;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "About";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(351, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "DNA→aa",
+            "aa→DNA"});
+            this.comboBox1.Location = new System.Drawing.Point(95, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Convert Type :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 281);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(400, 200);
+            this.Name = "Form1";
+            this.Text = "DNA&Protein sequence Converter";
+            this.Resize += new System.EventHandler(this.Form1_Resize_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ BiyoenformatikOdev2/BiyoenformatikOdev2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BiyoenformatikOdev2
+{
+    public partial class Form1 : Form
+    {
+        private string[] codons ={"TTT", "TTC", "TTA", "TTG", "TCT","TCC", "TCA", "TCG", "TAT", "TAC", "TAA", "TAG", "TGT", "TGC", "TGA", "TGG",
+                                  "CTT","CTC", "CTA", "CTG", "CCT", "CCC", "CCA", "CCG", "CAT", "CAC", "CAA", "CAG", "CGT", "CGC", "CGA", "CGG",
+                                  "ATT", "ATC", "ATA", "ATG", "ACT", "ACC", "ACA", "ACG", "AAT", "AAC", "AAA", "AAG","AGT", "AGC", "AGA", "AGG",
+                                  "GTT", "GTC", "GTA", "GTG", "GCT", "GCC", "GCA", "GCG", "GAT", "GAC", "GAA", "GAG", "GGT", "GGC", "GGA", "GGG"};
+
+        private string[] aminos = {"F", "F", "L", "L", "S", "S", "S", "S", "Y", "Y", "*", "*", "C", "C", "*", "W",
+                                   "L", "L", "L", "L", "P", "P", "P", "P", "H", "H", "Q", "Q", "R", "R", "R", "R",
+                                   "I", "I", "I", "M", "T", "T", "T", "T", "N", "N", "K", "K", "S", "S", "R", "R",
+                                   "V", "V", "V", "V", "A", "A", "A", "A", "D", "D", "E", "E", "G","G", "G", "G", };
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        bool text_correct;
+        string codon;
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            codon = "";
+            text_correct = true;
+            
+            if(comboBox1.SelectedIndex == 0)
+            {
+                foreach (char c in richTextBox1.Text.Replace("U", "T"))
+                {
+                    if (c != Convert.ToChar("A") && c != Convert.ToChar("C") && c != Convert.ToChar("G") && c != Convert.ToChar("T"))
+                    {
+                        text_correct = false;
+                        break;
+                    }
+
+                    codon += c;
+
+                    if(codon.Length == 3)
+                    {
+                        richTextBox2.Text = richTextBox2.Text + aminos[Array.IndexOf(codons, codon)];
+                        codon = "";
+                    }
+                }
+
+                if (text_correct == false)
+                    richTextBox2.Text = "*Unexpected character input, please check your input!\n\nNot:The Input should be a DNA code like AATCCGAGGCT (like FLVHNDIA for amino acids) without any blank, blank line, lower case or other input.";
+            }
+            else
+            {
+                foreach (char c in richTextBox1.Text)
+                {
+                    if (Array.IndexOf(aminos, Convert.ToString(c)) == -1)
+                    {
+                        text_correct = false;
+                        break;
+                    }
+                    
+                    richTextBox2.Text = richTextBox2.Text + codons[Array.IndexOf(aminos, Convert.ToString(c))];
+                }
+
+                if (text_correct == false)
+                    richTextBox2.Text = "*Unexpected character input, please check your input!\n\nNot:The Input should be a DNA code like AATCCGAGGCT (like FLVHNDIA for amino acids) without any blank, blank line, lower case or other input.";
+            }
+        }
+        
+        //Down just about GUI
+        private void Form1_Resize_1(object sender, EventArgs e)
+        {
+            richTextBox1.Width = this.Width / 2 - 20;
+            richTextBox2.Width = this.Width / 2 - 20;
+            richTextBox1.Height = this.Height -100;
+            richTextBox2.Height = this.Height -100;
+
+            richTextBox2.Location = new Point(richTextBox1.Location.X + richTextBox1.Width + 5, richTextBox2.Location.Y);
+            linkLabel1.Location = new Point(this.Width - 50, 9);
+            linkLabel2.Location = new Point(this.Width - 50, this.Height - 55);
+            
+            label2.Location = new Point(richTextBox2.Location.X, label2.Location.Y);
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("The application converts DNA sequence or protein sequence to other one. For action just need to enter inputs to left box. The application automatically detects the input and convert it to other one.");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("Coded by ibrahim Kocak");
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+    }
+}
